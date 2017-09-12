24a8a175ed5bbc374e2f797274b94b5bd38ae1ef
lowbees <demoresh@126.com>
2017-09-13 02:09:52 +0800

    采用一种更佳的方式

--- main.cpp
+++ main.cpp
@@ -8,14 +8,12 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
     QGuiApplication app(argc, argv);
 
+    qmlRegisterType<FramelessWindowHelper>("FramelessWindowHelper", 1, 0, "FramelessWindowHelper");
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     if (engine.rootObjects().isEmpty())
         return -1;
-    FramelessWindowHelper helper(&engine);
 
-    // 主窗口必须以这种方式
-    helper.addWindow("MainWindow");
     return app.exec();
 }
